c4a974d4cb42a7a65596628426a8923d704dc31d
zhengxing Lu <1054340380@qq.com>
2019-02-26 15:36:01 +0800

    arduino example

--- examples/BurstRead_demo/BurstRead_demo.ino
+++ examples/BurstRead_demo/BurstRead_demo.ino
@@ -31,9 +31,9 @@
 #include <Wire.h>
 
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
-  #define SERIAL SerialUSB
+    #define SERIAL SerialUSB
 #else
-  #define SERIAL Serial
+    #define SERIAL Serial
 #endif
 
 uint8_t burstdata[20];
@@ -50,88 +50,85 @@ float TEMPS = 0;
 // Call ADIS16470 Class
 ADIS16470 IMU(10);
 
-void scaleData()
-{
-  GXS = IMU.gyroScale(*(wordData + 1)); //Scale X Gyro
-  GYS = IMU.gyroScale(*(wordData + 2)); //Scale Y Gyro
-  GZS = IMU.gyroScale(*(wordData + 3)); //Scale Z Gyro
-  AXS = IMU.accelScale(*(wordData + 4)); //Scale X Accel
-  AYS = IMU.accelScale(*(wordData + 5)); //Scale Y Accel
-  AZS = IMU.accelScale(*(wordData + 6)); //Scale Z Accel
-  TEMPS = IMU.tempScale(*(wordData + 7)); //Scale Temp Sensor
+void scaleData() {
+    GXS = IMU.gyroScale(*(wordData + 1)); //Scale X Gyro
+    GYS = IMU.gyroScale(*(wordData + 2)); //Scale Y Gyro
+    GZS = IMU.gyroScale(*(wordData + 3)); //Scale Z Gyro
+    AXS = IMU.accelScale(*(wordData + 4)); //Scale X Accel
+    AYS = IMU.accelScale(*(wordData + 5)); //Scale Y Accel
+    AZS = IMU.accelScale(*(wordData + 6)); //Scale Z Accel
+    TEMPS = IMU.tempScale(*(wordData + 7)); //Scale Temp Sensor
 }
 
-void setup() 
-{
-  Wire.begin();        // join i2c bus (address optional for master)
-  SERIAL.begin(9600);  // start serial for output
+void setup() {
+    Wire.begin();        // join i2c bus (address optional for master)
+    SERIAL.begin(9600);  // start serial for output
 }
 
 void loop() 
 {
-  Wire.requestFrom(0x36, 20);    // request 6 bytes from slave device #8
-  int i=0;
-  while (Wire.available()) 
-  { // slave may send less than requested
-    uint8_t c = Wire.read(); // receive a byte as character
-    burstdata[i] = c;
-    i++;
-    //SERIAL.print(c,HEX);
-    //SERIAL.print("  ");
-  }
-  i=0;
-
-  wordData = IMU.wordData(burstdata);
-  scaleData();
-
-  //print the array of burstread and checksum
-  /*
-    int s=0;
-    for(int i=0;i<20;i++)
-    {
-        if(i<18)
-        {
-            s+=burstdata[i];
-        }
-        else
-        {
-            burstChecksum = s;
-        }
-        
-        SERIAL.print(burstdata[i],HEX);
-        SERIAL.print("  ");
-        if(i%5==0)SERIAL.println();
+    Wire.requestFrom(0x36, 20);    // request 6 bytes from slave device #8
+    int i=0;
+    while (Wire.available()) { // slave may send less than requested
+      uint8_t c = Wire.read(); // receive a byte as character
+      burstdata[i] = c;
+      i++;
+      //SERIAL.print(c,HEX);
+      //SERIAL.print("  ");
     }
-    SERIAL.print("checksum: ");
-    SERIAL.println(burstChecksum,HEX);
-  */
-  SERIAL.println("Version:v1.0");
-  // Print scaled gyro data
-  SERIAL.print("XGYRO(degrees/sec): ");
-  SERIAL.println(GXS);
-  SERIAL.print("YGYRO(degrees/sec): ");
-  SERIAL.println(GYS);
-  SERIAL.print("ZGYRO(degrees/sec): ");
-  SERIAL.println(GZS);
-  
-  // Print scaled accel data
-  SERIAL.print("XACCL(g's): ");
-  SERIAL.println(AXS);
-  SERIAL.print("YACCL(g's): ");
-  SERIAL.println(AYS);
-  SERIAL.print("ZACCL(g's): ");
-  SERIAL.println(AZS);
-  SERIAL.println(" ");
+    i = 0;
 
-  // Print Status Registers
-  SERIAL.print("DIAG_STAT: ");
-  SERIAL.println((*(burstdata + 0)));
-  SERIAL.print("TIME_STMP: ");
-  SERIAL.println((*(burstdata + 8)));
-  SERIAL.print("CHECKSUM: ");
-  SERIAL.println((*(burstdata + 9)));
-  
-  SERIAL.println();
-  SERIAL.println();
-  //delay(2000);
+    wordData = IMU.wordData(burstdata);
+    scaleData();
+
+    //print the array of burstread and checksum
+    /*
+      int s = 0;
+      for(int i = 0; i < 20; i++)
+      {
+          if(i < 18)
+          {
+              s += burstdata[i];
+          }
+          else
+          {
+              burstChecksum = s;
+          }
+
+          SERIAL.print(burstdata[i], HEX);
+          SERIAL.print("  ");
+          if(i%5 == 0)SERIAL.println();
+      }
+      SERIAL.print("checksum: ");
+      SERIAL.println(burstChecksum, HEX);
+    */
+    SERIAL.println("Version:v1.0");
+    // Print scaled gyro data
+    SERIAL.print("XGYRO(degrees/sec): ");
+    SERIAL.println(GXS);
+    SERIAL.print("YGYRO(degrees/sec): ");
+    SERIAL.println(GYS);
+    SERIAL.print("ZGYRO(degrees/sec): ");
+    SERIAL.println(GZS);
+
+    // Print scaled accel data
+    SERIAL.print("XACCL(g's): ");
+    SERIAL.println(AXS);
+    SERIAL.print("YACCL(g's): ");
+    SERIAL.println(AYS);
+    SERIAL.print("ZACCL(g's): ");
+    SERIAL.println(AZS);
+    SERIAL.println(" ");
+
+    // Print Status Registers
+    SERIAL.print("DIAG_STAT: ");
+    SERIAL.println((*(burstdata + 0)));
+    SERIAL.print("TIME_STMP: ");
+    SERIAL.println((*(burstdata + 8)));
+    SERIAL.print("CHECKSUM: ");
+    SERIAL.println((*(burstdata + 9)));
+
+    SERIAL.println();
+    SERIAL.println();
+    //delay(2000);
 }
